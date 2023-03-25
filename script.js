dde23fde4b40cb3fd7d2b6f7721c7d0937d73bf4
kodego-mae <funj451@gmail.com>
2023-03-25 13:17:00 +0700

    finalize files

--- script.js
+++ script.js
@@ -1,16 +1,68 @@
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection.toLowerCase() == "rock" &&
+      computerSelection[computerPlay] == "scissors") ||
+    (playerSelection.toLowerCase() == "paper" &&
+      computerSelection[computerPlay] == "rock") ||
+    (playerSelection.toLowerCase() == "scissors" &&
+      computerSelection[computerPlay] == "paper")
+  ) {
+    playerScore++;
+    return win;
+  } else if (
+    (playerSelection.toLowerCase() == "rock" &&
+      computerSelection[computerPlay] == "paper") ||
+    (playerSelection.toLowerCase() == "scissors" &&
+      computerSelection[computerPlay] == "rock") ||
+    (playerSelection.toLowerCase() == "paper" &&
+      computerSelection[computerPlay] == "scissors")
+  ) {
+    computerScore++;
+    return lose;
+  } else if (playerSelection.toLowerCase() == computerSelection[computerPlay]) {
+    tieScore++;
+    return tie;
+  } 
+  else {
+    return "Error please input rock, paper or scissors";
+  } /
+}
+
+let playerSelection = prompt("Please input rock, paper or scissors"); 
+
+console.log("you choose " + playerSelection.toLowerCase()); 
+
+let computerSelection = ["rock", "paper", "scissors"];
+let computerPlay = Math.floor(Math.random() * computerSelection.length); 
+console.log(
+  "The computer choose " + computerPlay,
+  computerSelection[computerPlay]
+);
+
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
+
+const win = "You Win this round!\n -------"; 
+const lose = "You Lose this round!\n -------"; 
+const tie = "This round is a tie!\n -------"; 
+
+console.log(playRound(playerSelection, computerSelection));
+
 function game() {
   for (let i = 0; i < 4; i++) {
-    playerSelection = prompt("Lets play! Enter rock, papper or scissors"); //define player, player input
 
-    console.log("you choose " + playerSelection.toLowerCase()); //show user input in lowercase
+    playerSelection = prompt("lets play rock papper scissors"); 
 
-    computerSelection = ["rock", "paper", "scissors"]; //update computerSelection
-    computerPlay = Math.floor(Math.random() * computerSelection.length); //update computerPlay
+    console.log("you choose " + playerSelection.toLowerCase());
+
+    computerSelection = ["rock", "paper", "scissors"]; 
+    computerPlay = Math.floor(Math.random() * computerSelection.length); 
 
     console.log(
       "The computer choose " + computerPlay,
       computerSelection[computerPlay]
-    );
+    ); 
 
     console.log(playRound(playerSelection, computerSelection));
   }
