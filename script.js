e23d5e6ebd2f8353121640ec08800ee93a97ec31
kodego-mae <funj451@gmail.com>
2023-03-25 13:17:43 +0700

    final upadated file

--- script.js
+++ script.js
@@ -22,18 +22,17 @@ function playRound(playerSelection, computerSelection) {
   } else if (playerSelection.toLowerCase() == computerSelection[computerPlay]) {
     tieScore++;
     return tie;
-  } 
-  else {
+  } else {
     return "Error please input rock, paper or scissors";
-  } /
+  }
 }
 
-let playerSelection = prompt("Please input rock, paper or scissors"); 
+let playerSelection = prompt("Please input rock, paper or scissors");
 
-console.log("you choose " + playerSelection.toLowerCase()); 
+console.log("you choose " + playerSelection.toLowerCase());
 
 let computerSelection = ["rock", "paper", "scissors"];
-let computerPlay = Math.floor(Math.random() * computerSelection.length); 
+let computerPlay = Math.floor(Math.random() * computerSelection.length);
 console.log(
   "The computer choose " + computerPlay,
   computerSelection[computerPlay]
@@ -43,26 +42,25 @@ let playerScore = 0;
 let computerScore = 0;
 let tieScore = 0;
 
-const win = "You Win this round!\n -------"; 
-const lose = "You Lose this round!\n -------"; 
-const tie = "This round is a tie!\n -------"; 
+const win = "You Win this round!\n -------";
+const lose = "You Lose this round!\n -------";
+const tie = "This round is a tie!\n -------";
 
 console.log(playRound(playerSelection, computerSelection));
 
 function game() {
   for (let i = 0; i < 4; i++) {
-
-    playerSelection = prompt("lets play rock papper scissors"); 
+    playerSelection = prompt("lets play rock papper scissors");
 
     console.log("you choose " + playerSelection.toLowerCase());
 
-    computerSelection = ["rock", "paper", "scissors"]; 
-    computerPlay = Math.floor(Math.random() * computerSelection.length); 
+    computerSelection = ["rock", "paper", "scissors"];
+    computerPlay = Math.floor(Math.random() * computerSelection.length);
 
     console.log(
       "The computer choose " + computerPlay,
       computerSelection[computerPlay]
-    ); 
+    );
 
     console.log(playRound(playerSelection, computerSelection));
   }
